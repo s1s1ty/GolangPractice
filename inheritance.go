08e93e426070fa3b2046a424e3cf8704ebd57e5e
Pathao <pathao@Shaontys-Mac.local>
2018-02-04 18:40:48 +0600

    updated

--- inheritance.go
+++ inheritance.go
@@ -2,50 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-var months = map[string]int{
-	"January":   31,
-	"February":  28,
-	"March":     31,
-	"April":     30,
-	"May":       31,
-	"June":      30,
-	"July":      31,
-	"August":    31,
-	"September": 30,
-	"October":   31,
-	"November":  30,
-	"December":  31,
+type author struct {
+	firstName string
+	lastName  string
+	bio       string
 }
 
-type Person struct {
-	name string
-	age  int
+func (a author) fullname() string {
+	return fmt.Sprintf("%s %s", a.firstName, a.lastName)
 }
 
-type Account struct {
-	Person           //anonymous field
-	account_name     string
-	time_spent_daily int
+type book struct {
+	name   string
+	author // embeded type
 }
 
-func (a1 Account) SpentTime() int {
-	current_month := time.Now().Month()
-	days := months[current_month.String()]
-	total_time := days * a1.time_spent_daily
-	return total_time
-
+func (b book) bookInfo() {
+	fmt.Println("Book Name:", b.name)
+	fmt.Println("Author Name:", b.fullname())
+	fmt.Println("Author Bio:", b.bio)
 }
 
 func main() {
-	a1 := Account{Person{"Shaon", 100}, "Facebook", 2}
-	a2 := Account{Person{"Munni", 100}, "Youtube", 4}
-
-	fmt.Printf("%s will take %d hours in this month from %s's life.\n",
-		a1.account_name, a1.SpentTime(), a1.name)
-
-	fmt.Printf("%s will take %d hours in this month from %s's life.\n",
-		a2.account_name, a2.SpentTime(), a2.name)
+	author1 := author{
+		"shaon",
+		"shaonty",
+		"no bio",
+	}
+	book1 := book{
+		"Go is awesome",
+		author1,
+	}
+	book1.bookInfo()
 }
---
+++ polymorphism.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+const PI float32 = 3.1416
+
+type circle struct {
+	redius float32
+}
+
+func (c circle) area() float32 {
+	return PI * (c.redius * c.redius)
+}
+
+type trapezium struct {
+	baseA  float32
+	baseB  float32
+	height float32
+}
+
+func (t trapezium) area() float32 {
+	return ((t.baseA + t.baseB) / 2) * t.height
+}
+
+type ellipse struct {
+	a float32
+	b float32
+}
+
+func (e ellipse) area() float32 {
+	return PI * e.a * e.b
+}
+
+func main() {
+	c := circle{redius: 4}
+	t := trapezium{8, 10, 4}
+	e := ellipse{2, 3}
+
+	fmt.Println("Area of Circle: ", c.area())
+	fmt.Println("Area of Trapezium: ", t.area())
+	fmt.Println("Area of ellipse: ", e.area())
+}
